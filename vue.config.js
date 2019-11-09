884dc7c513a474e0377b6d1f9c644c074dc2cbbf
52326125 <zxc5328032@gmail.com>
2019-11-09 11:28:52 +0800

    add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,23 @@
 module.exports = {
-    configureWebpack: {
-      devtool: 'source-map',
-      devServer: {
-        port: process.env.port,
-        open: true,
-        public: 'localhost:8080',
-        proxy: {
-          '/api': {
-            target: 'http://49.158.32.227:3000',
-            changeOrigin: true,
-            ws: true,
-            pathRewrite: {
-              '^/api': '/api'
-            },
-            'secure': false
-          }
+  "configureWebpack": {
+    "devtool": "source-map",
+    "devServer": {
+      "open": true,
+      "public": "localhost:8080",
+      "proxy": {
+        "/api": {
+          "target": "http://49.158.32.227:3000",
+          "changeOrigin": true,
+          "ws": true,
+          "pathRewrite": {
+            "^/api": "/api"
+          },
+          "secure": false
         }
       }
     }
-  }
-  
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
