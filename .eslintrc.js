f0931d9ef9cb6eea00e0170adadf1c7994b44385
52326125 <zxc5328032@gmail.com>
2021-01-06 09:25:15 +0800

    Cancel eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,14 @@ module.exports = {
     //'@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'semi': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'object-curly-spacing': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'key-spacing': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-spacing': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-trailing-spaces': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'quotes': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'semi': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'object-curly-spacing': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'key-spacing': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'comma-spacing': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-trailing-spaces': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'quotes': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
